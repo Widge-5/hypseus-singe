b7b76093afd6cf32976982225dbbab04e3473ef8
Widge (The real) <77170359+Widge-5@users.noreply.github.com>
2026-05-12 20:44:05 +0100

    Improve error handling for SDL Mixer initialization

--- src/game/singe/singeproxy.cpp
+++ src/game/singe/singeproxy.cpp
@@ -516,9 +516,9 @@ static bool sep_init_mixer()
     SDL_setenv("SDL_SOUNDFONTS", "midi/soundfont.sf2", 1);
 
     if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
-        sep_die("SDL Mixer failed to open audio devices.");
+        sep_die("SDL Mixer failed to open audio devices: %s", SDL_GetError());
         return false;
-    }
+	}
 
     int flags = 0;
 
